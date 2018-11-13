6d0cc4d99158813e31ed629ed0722e3a2c41831d
monkfungus <sam_n@icloud.com>
2018-11-13 16:20:38 +0000

    Add SimpleServer

---
+++ SimpleServer/index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+app.use(bodyParser.json())
+
+app.get('/', (req, res) => {
+    res.send('hello')
+    console.log(req.method + req.url)
+})
+
+app.post('/', (req, res) => {
+    console.log(req.method + req.url)
+    console.log(req.body)
+    res.send('200')
+})
+
+app.listen(3000, () => {
+    console.log('Server running on port 3000')
+})
