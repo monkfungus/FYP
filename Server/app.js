44a0ad0786045f1218f71ef402bbadb1dd0c2f5a
monkfungus <sam_n@icloud.com>
2019-01-11 15:27:09 +0000

    Write straight through to DynamoDB!

--- Server/app.js
+++ Server/app.js
@@ -2,6 +2,14 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
 app.use(bodyParser.json())
+const AWS = require('aws-sdk')
+
+AWS.config.update({
+    endpoint: 'https://dynamodb.us-east-2.amazonaws.com',
+    region: 'us-east-2'
+})
+const docClient= new AWS.DynamoDB.DocumentClient()
+const tablename = 'readings'
 
 app.get('/', (req, res) => {
 	res.send('hello')
@@ -11,7 +19,19 @@ app.get('/', (req, res) => {
 app.post('/', (req, res) => {
 	console.log(req.method + req.url)
 	console.log(req.body)
-	res.sendStatus(200)
+	const params = {
+		TableName: tablename,
+		Item: req.body
+	}
+	docClient.put(params, function(err, data) {
+		if (err) {
+			console.error('Unable to add item. Error JSON: ', JSON.stringify(err, null, 2))
+			res.sendStatus(500)
+		} else {
+			console.log('Added item:', JSON.stringify(data, null, 2))
+			res.sendStatus(201)
+		}
+	})
 })
 
 // do the listen!
