2aa05552ea87e97e105fa2109fbcdd7902da6b09
monkfungus <sam_n@icloud.com>
2019-01-25 15:11:07 +0000

    Pi scans for devices and GPS for eternity

--- RaspberryPi/scan.py
+++ RaspberryPi/scan.py
@@ -2,44 +2,70 @@ import bluetooth
 import requests
 import json
 from datetime import datetime, timezone
+import time
+import board
+import busio
+import adafruit_gps
+import serial
 
 url = 'https://5o6pmquqyi.execute-api.us-east-2.amazonaws.com/latest/readings'
+RX = 10 # Pins for serial port
+TX = 8
+readFreq = 5.0
 
-# find nearby devices and save
-print('Scanning...')
-nearby_devices = bluetooth.discover_devices(
-        duration=8, lookup_names=True, flush_cache=True)
+# create serial connection to GPS
+uart = serial.Serial("/dev/ttyS0", baudrate=9600, timeout=3000)
 
-print('Found %d devices' % len(nearby_devices))
+# gps module instance
+gps = adafruit_gps.GPS(uart, debug=False)
+# Turn on the basic GGA and RMC info
+gps.send_command(b'PMTK314,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0')
+# Set update rate
+gps.send_command(b'PMTK220,1000')
 
-devices = []
-for addr, name in nearby_devices:
-    newDevice = {}
-    newDevice['address'] = addr
-    newDevice['name'] = name
-    devices.append(newDevice)
-    
-# mock location
-location = {}
-location['latitude'] = 83
-location['longitude'] = -9
+# run forever
+last = time.monotonic()
+while True:
+    gps.update()
+    current = time.monotonic()
+    if current - last > readFreq:
+        last = current
+        if not gps.has_fix:
+            print('gps has no fix')
+            continue
+        # have a fix
+        # find nearby devices and save
+        print('Scanning for Bluetooth devices...')
+        nearby_devices = bluetooth.discover_devices(
+                duration=8, lookup_names=True, flush_cache=True)
 
-# mock timestamp
-timestamp = datetime.now(timezone.utc)
+        print('Found %d devices' % len(nearby_devices))
 
-# build payload
-payload = {}
-payload['timestamp'] = timestamp
-payload['location'] = location
-payload['devices'] = devices
+        devices = []
+        for addr, name in nearby_devices:
+            newDevice = {}
+            newDevice['address'] = addr
+            newDevice['name'] = name
+            devices.append(newDevice)
+            
+        location = {}
+        location['latitude'] = gps.latitude
+        location['longitude'] = gps.longitude
 
-print(payload)
+        timestamp = datetime.now(timezone.utc).isoformat()
+        
+        # build payload
+        payload = {}
+        payload['timestamp'] = timestamp
+        payload['location'] = location
+        payload['devices'] = devices
 
-print('Sending to server..')
-try:
-    r = requests.post(url, json=payload)
-except requests.exceptions.ConnectionError as e:
-    print('ConnectionError: ', e)
-else:
-    print('Status: %s', r.status_code)
+        print(payload)
 
+        print('Sending to server..')
+        try:
+            r = requests.post(url, json=payload)
+        except requests.exceptions.ConnectionError as e:
+            print('ConnectionError: ', e)
+        else:
+            print('Status: %s', r.status_code)
