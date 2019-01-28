e905b8873df5c04b5119d319015969d0a2d9ded6
monkfungus <sam_n@icloud.com>
2019-01-28 13:43:17 +0000

    Begin converting Lambda function from claudia-api-builder to express

--- Lambda/index.js
+++ Lambda/index.js
@@ -1,19 +1,30 @@
 const ApiBuilder = require('claudia-api-builder')
-const AWS = require('aws-sdk')
 let api = new ApiBuilder()
+const express = require('express')
+const app = express()
+const AWS = require('aws-sdk')
 const docClient = new AWS.DynamoDB.DocumentClient()
 // TODO: move to env
 const tablename = 'readings'
 
-api.post('/readings', function (request) {
+app.post('/readings', function (req, res) {
     const params = {
         TableName: tablename,
         Item: request.body
     }
     return docClient.put(params).promise()
 }, { success: 201} )
-api.get('/hello', function (request) {
-    return 'hello there'
+// return all devices or specific devices
+app.get('/devices', function (req, res) {
+    if (request.queryString.name !== NaN) {
+        
+    }
+})
+app.get('/hello', (req, res) => {
+    res.send('hello there')
 })
 
-module.exports = api
+// comment out for deployment to AWS Lambda
+const port = 3000
+app.listen(port, () => console.log(`Listening on port ${port}`))
+module.exports = app
