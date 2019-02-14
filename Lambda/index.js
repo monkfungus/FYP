810968c0d6ff86b7d29c16fce326f97ea3dbb7cd
monkfungus <sam_n@icloud.com>
2019-02-14 17:54:39 +0000

    Beginnings of attempts to move back to express and add saving of unique devices

--- Lambda/index.js
+++ Lambda/index.js
@@ -5,22 +5,67 @@ const app = express()
 const AWS = require('aws-sdk')
 const docClient = new AWS.DynamoDB.DocumentClient()
 // TODO: move to env
-const tablename = 'readings'
+const table = {
+    readings: 'readings',
+    devices: 'devices'
+}
 
-app.post('/readings', function (req, res) {
-    const params = {
-        TableName: tablename,
+app.post('/readings', (req, res) => {
+    let params = {
+        TableName: table.readings,
         Item: request.body
     }
-    return docClient.put(params).promise()
-}, { success: 201} )
-// return all devices or specific devices
-app.get('/devices', function (req, res) {
-    if (request.queryString.name !== NaN) {
-        
-    }
+    // what happens here - when does response from docClient come back?
+    docClient.put(params).promise()
+        .then( value => {
+            // for each device:
+                // check if device already recorded in table, if not add it 
+                // if device already in devices
+                    // update location
+                // else 
+                    // add device with current location
+                    // update location
+            request.devices.array.forEach(element => {
+                params = {
+                    TableName: table.devices,
+                    KeyConditionExpression: "id = :id",
+                    ExpressionAttributeValues: {
+                        ":id": element.address
+                    }
+                }
+                docClient.query(params, function(err, data) {
+                    if (err) {
+                        console.err("Unable to get ")
+                        reject(err)
+                    } else {
+
+                    }
+                })
+            }); 
+            return res.sendStatus(201)
+        })
+        .then( value => {
+            return res.sendStatus(201)
+        })
+        .catch( err => {
+            // need more info
+            console.error('failed to put req in db')
+            res.sendStatus(500)
+        })
 })
-app.get('/hello', (req, res) => {
+// return list of devices or all details about specific device
+app.get('/devices', (req, res) => {
+    res.send('not yet implemented')
+})
+app.get('/devices/:id', (req, res) => {
+    console.log(req.params)
+    res.send('not yet implemented')
+})
+// // return estimation of current location
+// app.get('/devices:id/location', (req, res) => {
+    
+// })
+app.get('/', (req, res) => {
     res.send('hello there')
 })
 
